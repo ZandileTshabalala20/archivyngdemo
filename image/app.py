39630089e007313a08f30c6571e625e6c44e7742
ZandileTshabalala20 <zandiletsh20@gmail.com>
2024-04-10 00:20:43 +0200

    buildspec added

--- image/app.py
+++ image/app.py
@@ -3,3 +3,6 @@ print("Hello,World!")
 print("Current Dir is: ",os.getcwd()) #indicates that your current working directory
 print(os.listdir())
 print("Docker is set")
+
+#list the image
+"https://1.bp.blogspot.com/-Q86XGVJahug/Xm-aQfgtuMI/AAAAAAAAAcA/qqPQeihI39EbG2N87jG65xfnCdvF85-WQCLcBGAsYHQ/s1600/docker.png"
