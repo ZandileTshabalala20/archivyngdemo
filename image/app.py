6c582d1e76c3a9a0cd1cd8f0ca511147e386b513
ZandileTshabalala20 <64075295+ZandileTshabalala20@users.noreply.github.com>
2024-04-16 19:50:03 +0200

    Update app.py

--- image/app.py
+++ image/app.py
@@ -1,8 +1,17 @@
 import os
-print("Hello,World!")
-print("Current Dir is: ",os.getcwd()) #indicates that your current working directory
+
+# Print Hello, World!
+print("Hello, World!")
+
+# Print the current directory
+print("Current Dir is: ", os.getcwd())
+
+# List the contents of the current directory
 print(os.listdir())
+
+# Print Docker is set
 print("Docker is set")
 
-#list the image
-"https://1.bp.blogspot.com/-Q86XGVJahug/Xm-aQfgtuMI/AAAAAAAAAcA/qqPQeihI39EbG2N87jG65xfnCdvF85-WQCLcBGAsYHQ/s1600/docker.png"
+# Print the image URL
+docker_image_url = "https://1.bp.blogspot.com/-Q86XGVJahug/Xm-aQfgtuMI/AAAAAAAAAcA/qqPQeihI39EbG2N87jG65xfnCdvF85-WQCLcBGAsYHQ/s1600/docker.png"
+print("Docker image URL:", docker_image_url)
